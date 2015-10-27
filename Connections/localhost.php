71c1fc0576ca434a0595d5ff9f3b086aa9396351
123phpshop <service@123phpshop.com>
2015-10-27 16:53:07 +0800

    修改db链接信息

--- Connections/localhost.php
+++ Connections/localhost.php
@@ -4,9 +4,9 @@
 # Type="MYSQL"
 # HTTP="true"
 error_reporting(0); 
-$hostname_localhost = "localhost";
-$database_localhost = "123phpshopv12";
-$username_localhost = "root";
+$hostname_localhost = "";
+$database_localhost = "";
+$username_localhost = "";
 $password_localhost = "";
 $localhost = mysql_pconnect($hostname_localhost, $username_localhost, $password_localhost) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_query("set names utf8");
