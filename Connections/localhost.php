7be342985d76da4f6c1fd8207fec00b62adb3ad0
123phpshop <service@123phpshop.com>
2015-10-29 11:40:27 +0800

    修改bug，如果产品没有设置类型id的话，是不能设置属性的

--- Connections/localhost.php
+++ Connections/localhost.php
@@ -4,9 +4,9 @@
 # Type="MYSQL"
 # HTTP="true"
 error_reporting(0); 
-$hostname_localhost = "";
-$database_localhost = "";
-$username_localhost = "";
+$hostname_localhost = "localhost";
+$database_localhost = "123phpshopv14";
+$username_localhost = "root";
 $password_localhost = "";
 $localhost = mysql_pconnect($hostname_localhost, $username_localhost, $password_localhost) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_query("set names utf8");
