b3c9d15651086097cfec72bb16b6ad9d752ad870
123phpshop <service@123phpshop.com>
2015-12-10 20:22:06 +0800

    修改版权嘻嘻你

--- admin/privileges/add.php
+++ admin/privileges/add.php
@@ -1,4 +1,21 @@
-<?php require_once('../../Connections/localhost.php');  
+<?php 
+/**
+ * 123PHPSHOP
+ * ============================================================================
+ * 版权所有 2015 上海序程信息科技有限公司，并保留所有权利。
+ * 网站地址: http://www.123PHPSHOP.com；
+ * ----------------------------------------------------------------------------
+ * 这是一个免费的软件。您可以在商业目的和非商业目的地前提下对程序除本声明之外的
+ * 代码进行修改和使用；您可以对程序代码进行再发布，但一定请保留
+ * 本声明和上海序程信息科技有限公司的联系方式！本软件中使用到的第三方代码版权属
+ * 于原公司所有。上海序程信息科技有限公司拥有对本声明和123PHPSHOP软件使用的最终
+ * 解释权！
+ * ============================================================================
+ *  作者:	123PHPSHOP团队
+ *  手机:	13391334121
+ *  邮箱:	service@123phpshop.com
+ */
+ ?><?php require_once('../../Connections/localhost.php');  
 //require_once($_SERVER['DOCUMENT_ROOT'].'/Connections/lib/privileges.php');
 
 global $global_file_list_array;
@@ -215,14 +232,16 @@ $totalRows_privileges = mysql_num_rows($privileges);
   <p class="phpshop123_title"><?php echo isset($row_getParent['name'])?$row_getParent['name'].":":"";?> 权限列表</p>
    <table width="100%" border="1" class="phpshop123_list_box">
      <tr>
-       <th width="9%" scope="col">权限名称</th>
-       <th width="16%" scope="col">顺序</th>
-       <th width="75%" scope="col">操作</th>
+       <th width=" " scope="col">权限名称</th>
+       <th width=" " scope="col">顺序</th>
+       <th width="" scope="col">文件</th>
+       <th width="" scope="col">操作</th>
      </tr>
      <?php do { ?>
       <tr>
         <td><?php echo $row_privileges['name']; ?></td>
         <td><?php echo $row_privileges['sort']; ?></td>
+        <td><?php echo $row_privileges['file_name']; ?></td>
         <td><a href="remove.php?id=<?php echo $row_privileges['id']; ?>" onclick="return confirm('您确实要删除这个权限吗?');">删除</a> <a href="edit.php?id=<?php echo $row_privileges['id']; ?>">编辑</a></td>
       </tr>
       <?php } while ($row_privileges = mysql_fetch_assoc($privileges)); ?>
