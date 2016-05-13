22f16136e87e38d38a640905612ebba59c46448f
thomas <service@123phpshop.com>
2016-05-13 08:12:06 +0800

    ok,又重新开始了，修正了无法添加品牌的问题，天哪，怎么会有这个问题

--- admin/brands/add.php
+++ admin/brands/add.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * 123PHPSHOP
  * ============================================================================
@@ -15,76 +15,90 @@
  *  手机:	13391334121
  *  邮箱:	service@123phpshop.com
  */
- ?><?php require_once('../../Connections/localhost.php'); ?>
+?><?php
+
+require_once ('../../Connections/localhost.php');
+?>
 <?php
-$doc_url="brand.html#add";
-$support_email_question="添加品牌";
-log_admin($support_email_question);
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
 
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
+$doc_url = "brand.html#add";
+$support_email_question = "添加品牌";
+log_admin ( $support_email_question );
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") {
+	$theValue = (! get_magic_quotes_gpc ()) ? addslashes ( $theValue ) : $theValue;
+	
+	switch ($theType) {
+		case "text" :
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "long" :
+		case "int" :
+			$theValue = ($theValue != "") ? intval ( $theValue ) : "NULL";
+			break;
+		case "double" :
+			$theValue = ($theValue != "") ? "'" . doubleval ( $theValue ) . "'" : "NULL";
+			break;
+		case "date" :
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "defined" :
+			$theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+			break;
+	}
+	return $theValue;
 }
 
-$editFormAction = $_SERVER['PHP_SELF'];
-if (isset($_SERVER['QUERY_STRING'])) {
-  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+$editFormAction = $_SERVER ['PHP_SELF'];
+if (isset ( $_SERVER ['QUERY_STRING'] )) {
+	$editFormAction .= "?" . htmlentities ( $_SERVER ['QUERY_STRING'] );
 }
 
-if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
-
-// 我们这里需要对上传文件进行检查
-  include($_SERVER['DOCUMENT_ROOT'].'/Connections/lib/upload.php'); 
-  
-	$up = new fileupload;
-    //设置属性(上传的位置， 大小， 类型， 名是是否要随机生成)
-    $up -> set("path", $_SERVER['DOCUMENT_ROOT']."/uploads/brands/");
-    $up -> set("maxsize", 2000000);
-    $up -> set("allowtype", array("gif", "png", "jpg","jpeg"));
-    $up -> set("israndname", true);
-  
-    //使用对象中的upload方法， 就可以上传文件， 方法需要传一个上传表单的名子 pic, 如果成功返回true, 失败返回false
-    if($up->upload("image_path")) {
-       	$image_path="/uploads/brands/".$up->getFileName(); 
-	   
-		 $insertSQL = sprintf("INSERT INTO brands (name, image_path, url, sort, `desc`) VALUES (%s, %s, %s, %s, %s)",
-                       GetSQLValueString($_POST['name'], "text"),
-                       GetSQLValueString( $image_path, "text"),
-                       GetSQLValueString($_POST['url'], "text"),
-                       GetSQLValueString($_POST['sort'], "int"),
-                       GetSQLValueString($_POST['desc'], "text"));
-
-		  mysql_select_db($database_localhost, $localhost);
-		  $Result1 = mysql_query($insertSQL, $localhost) ;
-		  if(!$Result1){$logger->fatal("数据库操作失败:".$insertSQL);}
-
-		  $insertGoTo = "index.php";
-		  header(sprintf("Location: %s", $insertGoTo));
- 	}else {
-        //获取上传失败以后的错误提示
-        $error=$up->getErrorMsg();
-    }
-  }
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+if ((isset ( $_POST ["MM_insert"] )) && ($_POST ["MM_insert"] == "form1")) {
+	try {
+		// @todo 这里需要进行服务器方面参数的验证，如果验证不通过，那么告知
+		$insertSQL = sprintf ( "INSERT INTO brands (name, url, sort, `desc`) VALUES (%s,  %s, %s, %s)", GetSQLValueString ( $_POST ['name'], "text" ), GetSQLValueString ( $_POST ['url'], "text" ), GetSQLValueString ( $_POST ['sort'], "int" ), GetSQLValueString ( $_POST ['desc'], "text" ) );
+		// 如果有文件上传的话
+		if (! empty ( $_FILES ['image_path'] ['temp_name'] )) {
+			include ($_SERVER ['DOCUMENT_ROOT'] . '/Connections/lib/upload.php');
+			
+			$up = new fileupload ();
+			// 设置属性(上传的位置， 大小， 类型， 名是是否要随机生成)
+			$up->set ( "path", $_SERVER ['DOCUMENT_ROOT'] . "/uploads/brands/" );
+			$up->set ( "maxsize", 2000000 );
+			$up->set ( "allowtype", array (
+					"gif",
+					"png",
+					"jpg",
+					"jpeg" 
+			) );
+			$up->set ( "israndname", true );
+			
+			// 使用对象中的upload方法， 就可以上传文件， 方法需要传一个上传表单的名子 pic, 如果成功返回true, 失败返回false
+			if ($up->upload ( "image_path" )) {
+				$image_path = "/uploads/brands/" . $up->getFileName ();
+				$insertSQL = sprintf ( "INSERT INTO brands (name, image_path, url, sort, `desc`) VALUES (%s, %s, %s, %s, %s)", GetSQLValueString ( $_POST ['name'], "text" ), GetSQLValueString ( $image_path, "text" ), GetSQLValueString ( $_POST ['url'], "text" ), GetSQLValueString ( $_POST ['sort'], "int" ), GetSQLValueString ( $_POST ['desc'], "text" ) );
+			} else {
+				// 获取上传失败以后的错误提示
+				throw new Exception ( $up->getErrorMsg () );
+			}
+		}
+		
+		// 这里需要检查是否有问题，如果没有问题，那么执行添加品牌的操作
+		mysql_select_db ( $database_localhost, $localhost );
+		$Result1 = mysql_query ( $insertSQL, $localhost );
+		if (! $Result1) {
+			$logger->fatal ( "数据库操作失败:" . $insertSQL );
+		}
+		
+		// 开始跳转
+		$insertGoTo = "index.php";
+		header ( sprintf ( "Location: %s", $insertGoTo ) );
+	} catch ( Exception $ex ) {
+		$error = $ex->getMessage ();
+	}
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -93,44 +107,59 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
 </head>
 
 <body>
-<span class="phpshop123_title">添加品牌</span><div id="doc_help" style="display:inline;height:40px;line-height:50px;color:#CCCCCC;"><a style="color:#CCCCCC;margin-left:3px;" target="_blank" href="<?php echo isset($doc_url)?"http://www.123phpshop.com/doc/v1.5/".$doc_url:"http://www.123phpshop.com/doc/";?>">[文档]</a><a style="color:#CCCCCC;margin-left:3px;" target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1718101117&site=qq&menu=yes">[人工支持]</a><a href=mailto:service@123phpshop.com?subject=我在<?php echo $support_email_question;?>的时候遇到了问题，请支持 style="color:#CCCCCC;margin-left:3px;">[邮件支持]</a></div>
-<a href="index.php">
-<input style="float:right;" type="submit" name="Submit2" value="品牌列表" />
-</a>
-<form action="<?php echo $editFormAction; ?>" method="post" enctype="multipart/form-data" name="form1" id="form1">
-  <table align="center" class="phpshop123_form_box">
-    <tr valign="baseline">
-      <td nowrap align="right">名称:</td>
-      <td><input type="text" name="name" value="" size="32" maxlength="32">
-      *</td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right">图片:</td>
-      <td><input type="file" name="image_path" value="" size="32"></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right">网址:</td>
-      <td><input name="url" type="text" value="http://" size="32" maxlength="60"></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right">排序:</td>
-      <td><input name="sort" type="text" value="" size="32" maxlength="10"></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right" valign="top">介绍:</td>
-      <td><textarea name="desc" cols="50" rows="5"></textarea>
-      </td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap align="right">&nbsp;</td>
-      <td><input type="submit" value="添加"></td>
-    </tr>
-  </table>
-  <input type="hidden" name="MM_insert" value="form1">
-</form>
-<script language="JavaScript" type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
-<script language="JavaScript" type="text/javascript" src="/js/jquery.validate.min.js"></script>
-<script>
+	<span class="phpshop123_title">添加品牌</span>
+	<div id="doc_help"
+		style="display: inline; height: 40px; line-height: 50px; color: #CCCCCC;">
+		<a style="color: #CCCCCC; margin-left: 3px;" target="_blank"
+			href="<?php echo isset($doc_url)?"http://www.123phpshop.com/doc/v1.5/".$doc_url:"http://www.123phpshop.com/doc/";?>">[文档]</a><a
+			style="color: #CCCCCC; margin-left: 3px;" target="_blank"
+			href="http://wpa.qq.com/msgrd?v=3&uin=1718101117&site=qq&menu=yes">[人工支持]</a><a
+			href=mailto:service@123phpshop.com?subject=我在
+			<?php echo $support_email_question;?> 的时候遇到了问题，请支持
+			style="color: #CCCCCC; margin-left: 3px;">[邮件支持]</a>
+	</div>
+	<a href="index.php"> <input style="float: right;" type="submit"
+		name="Submit2" value="品牌列表" />
+	</a>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/admin/widgets/_error.php');?>
+<form action="<?php echo $editFormAction; ?>" method="post"
+		enctype="multipart/form-data" name="form1" id="form1">
+		<table align="center" class="phpshop123_form_box">
+			<tr valign="baseline">
+				<td nowrap align="right">名称:</td>
+				<td><input type="text" name="name" value="" size="32" maxlength="32">
+						*</td>
+			</tr>
+			<tr valign="baseline">
+				<td nowrap align="right">图片:</td>
+				<td><input type="file" name="image_path" value="" size="32"></td>
+			</tr>
+			<tr valign="baseline">
+				<td nowrap align="right">网址:</td>
+				<td><input name="url" type="text" value="http://" size="32"
+					maxlength="60"></td>
+			</tr>
+			<tr valign="baseline">
+				<td nowrap align="right">排序:</td>
+				<td><input name="sort" type="text" value="" size="32" maxlength="10"></td>
+			</tr>
+			<tr valign="baseline">
+				<td nowrap align="right" valign="top">介绍:</td>
+				<td><textarea name="desc" cols="50" rows="5"></textarea></td>
+			</tr>
+			<tr valign="baseline">
+				<td nowrap align="right">&nbsp;</td>
+				<td><input type="submit" value="添加"></td>
+			</tr>
+		</table>
+		<input type="hidden" name="MM_insert" value="form1">
+	
+	</form>
+	<script language="JavaScript" type="text/javascript"
+		src="/js/jquery-1.7.2.min.js"></script>
+	<script language="JavaScript" type="text/javascript"
+		src="/js/jquery.validate.min.js"></script>
+	<script>
 $().ready(function(){
 
 	$("#form1").validate({
